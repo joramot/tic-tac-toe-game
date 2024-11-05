2ffce0e839207478b60e673d91324123bf78fad3
Jose RM <mojica.raul@gmail.com>
2024-11-05 16:01:54 -0600

    The game board is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-import './App.css'
+import './index.css'
+
+const TURNS = {
+  X: 'X',
+  O: 'O'
+}
+
+const board = Array(9).fill(null)
 
 function App() {
 
-  return <h1>tic tac toe</h1>
+  return (
+    <main className="board">
+      <h1>tic tac toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <div className="cell" key={index}>
+                <span className="cell_content">
+                  {index}
+                </span>
+              </div>
+            )
+          })
+        }
+      </section>
+    </main>
+  )
 }
 
 export default App
